5e06448a22962bbcf213b4444399ec0eda3d06da
Ann <anna_sergevna@bk.ru>
2023-06-01 00:29:43 +0300

    practic_1

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 alert(1);
-alert(2);
+const number0films = +prompt("Сколько фильмов?", " ");
+
+const personalMoviesDB = {
+  count: number0films,
+  movies: {},
+  actors: {},
+  geners: [],
+  privat: false,
+};
+
+const a = prompt("Одина из посл фильмов", " ");
+const b = prompt("На сколько его оцените?", " ");
+const c = prompt("Одина из посл фильмов", " ");
+const d = prompt("На сколько его оцените", " ");
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB);
